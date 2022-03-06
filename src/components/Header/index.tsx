98a71cb75b1c191a79cbf190f12a35d5803b6eda
Dirosaki <diegosgomes27@gmail.com>
2022-03-06 18:12:20 -0300

    Add targetBlank in Header Anchor

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,8 +8,14 @@ function Header() {
 			<img src={logoIMG} alt="Logo Codelândia" height={32} />
 
 			<div>
-				<a href="https://github.com/Dirosaki">Meu Github</a>
-				<S.JoinCommunityButton href="https://discord.gg/wNCWTVuxyz">
+				<a href="https://github.com/Dirosaki" target="_blank" rel="noreferrer">
+					Meu Github
+				</a>
+				<S.JoinCommunityButton
+					href="https://discord.gg/wNCWTVuxyz"
+					target="_blank"
+					rel="noreferrer"
+				>
 					Entrar na comunidade
 				</S.JoinCommunityButton>
 			</div>
